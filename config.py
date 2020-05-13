cc13c2878953518f916fc41bc4b7fe3e21fc001c
gary-schulke <gary.schulke@outlook.com>
2020-05-12 20:37:59 -0700

    Initial Setup

---
+++ config.py
@@ -0,0 +1,20 @@
+# Enter your API key
+#google maps key
+#Project Name:	My Project 42048
+#Project ID:	gary-249402
+#API Key:	AIzaSyAsCy6oYZ0wOIpDyi9dkRfrgaJ8BgQ-D7w
+#		Use this key in your application by passing it with the key=API_KEY parameter.
+gkey = "AIzaSyASi4stopT9FLzMaRpQTV3miEVvLXKl3cs"
+
+# Open Weather Key
+wkey = "042140631b66d91bbdb048ed0e632c1c"
+
+
+
+#NY Times Key
+#Project: Bootcamp
+nytkey = "LuY9f5cEiw4UqweIQwCidoJY3geisGfL"
+
+# TriMet Key
+# Application name "TriMetApp"
+trimetkey = "DAD516A485E0B5E287769142D"
